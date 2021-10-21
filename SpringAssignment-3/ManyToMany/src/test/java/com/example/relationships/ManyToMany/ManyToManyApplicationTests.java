6aff9c3aacce51984181fcdeec03aae080f3d398
Mukul <howalemukul@gmail.com>
2021-10-21 22:18:31 +0530

    Many to many files created

---
+++ SpringAssignment-3/ManyToMany/src/test/java/com/example/relationships/ManyToMany/ManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.relationships.ManyToMany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
